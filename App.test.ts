fd8e0c2f84d75d600e5e2e528c0d9fd1eb023486
ewanbogdan <ibogdan@remedypointsolutions.com>
2023-09-30 17:22:34 +0300

    Add github workflow

---
+++ App.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from "bun:test";
+
+describe("math", () => {
+  test("add", () => {
+    expect(2 + 2).toEqual(4);
+  });
+
+  test("multiply", () => {
+    expect(2 * 2).toEqual(4);
+  });
+});
